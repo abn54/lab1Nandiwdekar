897293639e228b4ca9cd2705c36267222a5292f9
Aayudh Nandiwdekar <abn5496@psu.edu>
2024-09-05 11:34:11 -0400

    Adeddew

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {}
+
+    Motorcycle mc = new Motorcycle( "ldjsk773frhur383j6");
+    Motorcycle mc = new Motorcycle( "ldjsk773frhur383j6", _color:"red");
+}
